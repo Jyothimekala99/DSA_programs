5e44bc2becb7c54596efb31adb92eb45a19b157c
Jyothi <jyothimekala140@gmail.com>
2023-12-19 00:02:47 +0530

    kadanes

---
+++ kadanes.java
@@ -0,0 +1,20 @@
+public class kadanes{
+    public static int kadanesAlgorithm(int[] a){
+        int currentSum=0;
+        int maxSum=Integer.MIN_VALUE;
+        for(int i =0; i<a.length;i++){
+            currentSum+=a[i];
+            if(currentSum<=0){
+                currentSum=0;
+            }
+        maxSum=Math.max(maxSum,currentSum);
+        }
+    return maxSum;
+    }
+    public static void main(String ...args){
+        int[] a={-2,-3,4,-1,-2,1,5,-3};
+        int maxSum=kadanesAlgorithm(a);
+        System.out.println(maxSum);
+    }
+}
+    
