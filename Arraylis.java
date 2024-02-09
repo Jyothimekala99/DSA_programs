019bf61cc2ed05cb9d046aea80e8bee266653940
jyothiiiiii <146316691+jyothiiiiii@users.noreply.github.com>
2024-02-09 22:09:04 +0530

    Add files via upload

---
+++ Arraylis.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Arraylis{
+    public static void main(String args[]){
+        ArrayList<Integer> al= new ArrayList<>();
+        al.add(0);
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.add(4);
+        al.set(3,50);
+        al.remove(2);
+        System.out.println(al);
+        System.out.println(al.get(3));
+        System.out.println(al.size());
+        al.remove((Integer)(2));
+        for(int i=0;i<al.size();i++){
+            System.out.print(al.get(i)+ " ");
+       }
+    }
+}
